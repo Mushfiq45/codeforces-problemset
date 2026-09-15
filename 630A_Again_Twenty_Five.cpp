5fe9dda332fa2c328fb97d325cd03b71f8bcb565
NM_Mushfiq <136987799+Mushfiq45@users.noreply.github.com>
2026-09-15 23:52:08 +0600

    Add files via upload

---
+++ 630A_Again_Twenty_Five.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  double n;
+
+  cin >> n;
+
+  if (n == 1)
+  {
+    cout << "5";
+  }
+  
+  else{
+  cout << "25";
+  }
+  return 0;
+}
